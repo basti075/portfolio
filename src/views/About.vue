fad15326e605cf7d0e64029a148157b6ab600fbc
basti075 <bastian.holzer@uni-bayreuth.de>
2025-01-22 22:20:10 +0100

    test

--- src/views/About.vue
+++ src/views/About.vue
@@ -14,7 +14,7 @@
     </div>
 
     <div class="photo">
-      <img src="img/berg.jpg" alt="Avatar of John" /> 
+      <img src="img/berg.png" alt="Avatar of John" /> 
     </div>
 
     <div style="clear:both"></div>
