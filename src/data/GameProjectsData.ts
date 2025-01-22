8294363e0d6cee1e0be8194b9df6a7ea825e8de3
basti075 <bastian.holzer@uni-bayreuth.de>
2025-01-22 21:09:03 +0100

    Change Project Description

--- src/data/GameProjectsData.ts
+++ src/data/GameProjectsData.ts
@@ -1,8 +1,8 @@
 import ProjectData from '@/data/ProjectData.ts'
 
 export default [
-    new ProjectData("project-1", "Optimistic Chubby", "img/projects/project-1-icon.png", 
-    `
+    new ProjectData("project-1", "Loop", "img/projects/project-1-icon.png",
+        `
     <div class="paragraph">
      <strong>Optimistic Chubby</strong> is a thing of beauty that I am so proud of. It's available on Android because why not.
      <br/>Image by <a href="https://www.pexels.com/fr-fr/@knownasovan" target="_blank">OVAN</a>.
